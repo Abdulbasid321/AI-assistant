ce85298d302043c4a41031be62ccaa916ccc4920
abdulbasid <abdulbasidhussain9@gmail.com>
2025-07-25 18:11:19 +0100

    updat

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'https://a-iassistant-frontend-r1px.vercel.app'],
   credentials: true
 }));
 app.use(cookieParser());
